da2430f3abef3e52c2ce267d65a413d5ed533a24
高偉軒 <nick_kao@wei-fintech.com>
2020-12-22 03:53:01 +0800

    專案建立

---
+++ ForMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ForMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
